b97a34167390b6ba8b929e5b0e78b8778c336e31
Roberto <mitras25jpn@hotmail.com>
2021-06-03 14:00:08 -0300

    exercicio1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { JoinComponent } from './join/join.component';
+import { USComponent } from './us/us.component';
+import { PriceComponent } from './price/price.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JoinComponent,
+    USComponent,
+    PriceComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
